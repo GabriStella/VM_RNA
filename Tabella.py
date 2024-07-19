42d144ea982b33d714a3c0b0a5b2b33754c8a729
GabriStella <gabriele.stella98@hotmail.it>
2024-07-19 15:09:19 +0200

    DOVREI AVER CARICATO TUTTO ..

--- Tabella.py
+++ Tabella.py
@@ -1,49 +1,52 @@
-import pandas as pd
-import pyodbc
+import mysql.connector
 
-server = 'VDI-RNA\\SQLEXPRESS'  
-database = 'RNA'
-trusted_connection = 'yes'
-driver = 'ODBC Driver 17 for SQL Server'  
 
-conn_str = f'DRIVER={driver};SERVER={server};DATABASE={database};Trusted_Connection={trusted_connection};'
+config = {
+    'user': 'RNAuser',
+    'password': '7RKlo1StosGVSCnx',
+    'host': 'localhost',
+    'database': 'RNAuser'
+}
 
-conn = pyodbc.connect(conn_str)
 
-table_name = 'Open_Data'
-create_table_query = f'''
-    CREATE TABLE {table_name} (
-        [Identificativo Misura (CAR)] NVARCHAR(400),
-        [Titolo Misura] NVARCHAR(4000),
-        [Tipo Misura] NVARCHAR(1000),
-        [Norma Misura] NVARCHAR(4000),
-        [COR] INT,
-        [Titolo Progetto] NVARCHAR(4000),
-        [Descrizione] NVARCHAR(4000),
-        [Data Concessione] VARCHAR(100),
-        [Cup] NVARCHAR(4000),
-        [Atto Concessione] NVARCHAR(4000),
-        [Denominazione Beneficiario] NVARCHAR(4000),
-        [C.F. Beneficiario] NVARCHAR(4000),
-        [Dimensione Beneficiario] NVARCHAR(4000),
-        [Regione] NVARCHAR(4000),
-        [Autorità Concedente] NVARCHAR(3000),
-        [Numero di riferimento della misura] NVARCHAR(4000),
-        [Identificativo componente] INT,
-        [Tipo procedimento] NVARCHAR(4000),
-        [Regolamento/Comunicazione] NVARCHAR(4000),
-        [Obiettivo] NVARCHAR(4000),
-        [Settore di attività] NVARCHAR(4000),
-        [Strumento di aiuto] NVARCHAR(4000),
-        [Codice Strumento] NVARCHAR(4000),
-        [Elemento di aiuto] FLOAT,
-        [Importo Nominale] FLOAT ,
-        [Codice Univoco] NVARCHAR(4000)       
-    );
+conn = mysql.connector.connect(**config)
+cursor = conn.cursor()
+
+
+table_name = 'RNA_TAB'
+create_table_query = '''
+    CREATE TABLE RNA (
+        `Identificativo Misura (CAR)` VARCHAR(255),
+        `Titolo Misura` VARCHAR(1000),
+        `Tipo Misura` VARCHAR(255),
+        `Norma Misura` VARCHAR(1000),
+        `COR` INT,
+        `Titolo Progetto` VARCHAR(1000),
+        `Descrizione` TEXT,
+        `Data Concessione` VARCHAR(100),
+        `Cup` VARCHAR(255),
+        `Atto Concessione` VARCHAR(1000),
+        `Denominazione Beneficiario` VARCHAR(1000),
+        `C.F. Beneficiario` VARCHAR(255),
+        `Dimensione Beneficiario` VARCHAR(255),
+        `Regione` VARCHAR(255),
+        `Autorità Concedente` VARCHAR(1000),
+        `Numero di riferimento della misura` VARCHAR(1000),
+        `Identificativo componente` INT,
+        `Tipo procedimento` VARCHAR(1000),
+        `Regolamento/Comunicazione` VARCHAR(1000),
+        `Obiettivo` VARCHAR(1000),
+        `Settore di attività` VARCHAR(1000),
+        `Strumento di aiuto` VARCHAR(1000),
+        `Codice Strumento` VARCHAR(255),
+        `Elemento di aiuto` FLOAT,
+        `Importo Nominale` FLOAT,
+        `Codice Univoco` VARCHAR(255)
+    )
 '''
 
-cursor = conn.cursor()
 cursor.execute(create_table_query)
-conn.commit()
+print(f"Tabella {table_name} creata con successo.")
+
+cursor.close()
 conn.close()
-print(f'Tabella "{table_name}" creata con successo.')
